96861f19916841d99d932e01b2bce4cf6d050f27
Antonio Vilela Pereira <antonio.vilela.pereira@cern.ch>
2021-05-07 20:29:05 +0200

    Add processing scripts. Add implementation of JEC uncertainties.

--- ProcessData.py
+++ ProcessData.py
@@ -3,29 +3,33 @@ from processing import *
 import ROOT
 
 class ProcessData:
-    def __init__( self, labels, fileNames ):
+    def __init__( self, labels, fileNames, runOnMC=False ):
         self.labels_ = labels
         self.fileNames_ = fileNames
+        self.runOnMC_ = runOnMC
 
-        cmssw_release_base_ = os.environ[ 'CMSSW_RELEASE_BASE' ]
-        print ( cmssw_release_base_ )
-        cmssw_base_ = os.environ[ 'CMSSW_BASE' ]
-        print( cmssw_base_ )
+        self.jecPars_ = None
+        self.jecUncertainty_ = None
+        if self.runOnMC_:
+            cmssw_release_base_ = os.environ[ 'CMSSW_RELEASE_BASE' ]
+            print ( cmssw_release_base_ )
+            cmssw_base_ = os.environ[ 'CMSSW_BASE' ]
+            print( cmssw_base_ )
 
-        libraries_ = ROOT.gSystem.GetLibraries().split()
-        found_ = False
-        for item in libraries_:
-            if item.find( "libCondFormatsJetMETObjects.so" ) >= 0: found_ = True
+            libraries_ = ROOT.gSystem.GetLibraries().split()
+            found_ = False
+            for item in libraries_:
+                if item.find( "libCondFormatsJetMETObjects.so" ) >= 0: found_ = True
 
-        if not found_:
-            lib_path_ = cmssw_release_base_ + "/lib/slc7_amd64_gcc900/libCondFormatsJetMETObjects.so"
-            print ( "Loading {}".format( lib_path_ ) )
-            ROOT.gSystem.Load( lib_path_ )
+            if not found_:
+                lib_path_ = cmssw_release_base_ + "/lib/slc7_amd64_gcc900/libCondFormatsJetMETObjects.so"
+                print ( "Loading {}".format( lib_path_ ) )
+                ROOT.gSystem.Load( lib_path_ )
 
-        self.jecPars_ = ROOT.JetCorrectorParameters( cmssw_base_ + "/src/PhysicsTools/NanoAODTools/data/jme/" + "Fall17_17Nov2017_V32_MC_Uncertainty_AK8PFchs.txt" )
-        print ( self.jecPars_ )
-        self.jecUncertainty_ = ROOT.JetCorrectionUncertainty( self.jecPars_ )
-        print ( self.jecUncertainty_ )
+            self.jecPars_ = ROOT.JetCorrectorParameters( cmssw_base_ + "/src/PhysicsTools/NanoAODTools/data/jme/" + "Fall17_17Nov2017_V32_MC_Uncertainty_AK8PFchs.txt" )
+            print ( self.jecPars_ )
+            self.jecUncertainty_ = ROOT.JetCorrectionUncertainty( self.jecPars_ )
+            print ( self.jecUncertainty_ )
 
     def getJetUncertainty( self, jet_eta, jet_pt ):
         self.jecUncertainty_.setJetEta( jet_eta )
@@ -42,22 +46,23 @@ class ProcessData:
         df.loc[ :, "jet0_px" + label_ ]       = ( df.loc[ :, "jet0_pt" + label_ ] * np.cos( df.loc[ :, "jet0_phi" ] ) )
         df.loc[ :, "jet0_py" + label_ ]       = ( df.loc[ :, "jet0_pt" + label_ ] * np.sin( df.loc[ :, "jet0_phi" ] ) )
         df.loc[ :, "jet0_pz" + label_ ]       = ( df.loc[ :, "jet0_pt" + label_ ] * np.sinh( df.loc[ :, "jet0_eta" ] ) )
-        label_ = "_jes_up"
-        df.loc[ :, "jet0_pt" + label_ ]       = df.loc[ :, "jet0_pt" ] * ( 1. + df.loc[ :, "jet0_unc" ] ) 
-        df.loc[ :, "jet0_energy" + label_ ]   = df.loc[ :, "jet0_energy" ] * ( 1. + df.loc[ :, "jet0_unc" ] ) 
-        df.loc[ :, "jet0_mass" + label_ ]     = df.loc[ :, "jet0_mass" ] * ( 1. + df.loc[ :, "jet0_unc" ] )
-        df.loc[ :, "jet0_corrmass" + label_ ] = df.loc[ :, "jet0_corrmass" ] * ( 1. + df.loc[ :, "jet0_unc" ] )
-        df.loc[ :, "jet0_px" + label_ ]       = ( df.loc[ :, "jet0_pt" + label_ ] * np.cos( df.loc[ :, "jet0_phi" ] ) )
-        df.loc[ :, "jet0_py" + label_ ]       = ( df.loc[ :, "jet0_pt" + label_ ] * np.sin( df.loc[ :, "jet0_phi" ] ) )
-        df.loc[ :, "jet0_pz" + label_ ]       = ( df.loc[ :, "jet0_pt" + label_ ] * np.sinh( df.loc[ :, "jet0_eta" ] ) )
-        label_ = "_jes_dw"
-        df.loc[ :, "jet0_pt" + label_ ]       = df.loc[ :, "jet0_pt" ] * ( 1. - df.loc[ :, "jet0_unc" ] ) 
-        df.loc[ :, "jet0_energy" + label_ ]   = df.loc[ :, "jet0_energy" ] * ( 1. - df.loc[ :, "jet0_unc" ] ) 
-        df.loc[ :, "jet0_mass" + label_ ]     = df.loc[ :, "jet0_mass" ] * ( 1. - df.loc[ :, "jet0_unc" ] )
-        df.loc[ :, "jet0_corrmass" + label_ ] = df.loc[ :, "jet0_corrmass" ] * ( 1. - df.loc[ :, "jet0_unc" ] )
-        df.loc[ :, "jet0_px" + label_ ]       = ( df.loc[ :, "jet0_pt" + label_ ] * np.cos( df.loc[ :, "jet0_phi" ] ) )
-        df.loc[ :, "jet0_py" + label_ ]       = ( df.loc[ :, "jet0_pt" + label_ ] * np.sin( df.loc[ :, "jet0_phi" ] ) )
-        df.loc[ :, "jet0_pz" + label_ ]       = ( df.loc[ :, "jet0_pt" + label_ ] * np.sinh( df.loc[ :, "jet0_eta" ] ) )
+        if self.runOnMC_:
+            label_ = "_jes_up"
+            df.loc[ :, "jet0_pt" + label_ ]       = df.loc[ :, "jet0_pt" ] * ( 1. + df.loc[ :, "jet0_unc" ] ) 
+            df.loc[ :, "jet0_energy" + label_ ]   = df.loc[ :, "jet0_energy" ] * ( 1. + df.loc[ :, "jet0_unc" ] ) 
+            df.loc[ :, "jet0_mass" + label_ ]     = df.loc[ :, "jet0_mass" ] * ( 1. + df.loc[ :, "jet0_unc" ] )
+            df.loc[ :, "jet0_corrmass" + label_ ] = df.loc[ :, "jet0_corrmass" ] * ( 1. + df.loc[ :, "jet0_unc" ] )
+            df.loc[ :, "jet0_px" + label_ ]       = ( df.loc[ :, "jet0_pt" + label_ ] * np.cos( df.loc[ :, "jet0_phi" ] ) )
+            df.loc[ :, "jet0_py" + label_ ]       = ( df.loc[ :, "jet0_pt" + label_ ] * np.sin( df.loc[ :, "jet0_phi" ] ) )
+            df.loc[ :, "jet0_pz" + label_ ]       = ( df.loc[ :, "jet0_pt" + label_ ] * np.sinh( df.loc[ :, "jet0_eta" ] ) )
+            label_ = "_jes_dw"
+            df.loc[ :, "jet0_pt" + label_ ]       = df.loc[ :, "jet0_pt" ] * ( 1. - df.loc[ :, "jet0_unc" ] ) 
+            df.loc[ :, "jet0_energy" + label_ ]   = df.loc[ :, "jet0_energy" ] * ( 1. - df.loc[ :, "jet0_unc" ] ) 
+            df.loc[ :, "jet0_mass" + label_ ]     = df.loc[ :, "jet0_mass" ] * ( 1. - df.loc[ :, "jet0_unc" ] )
+            df.loc[ :, "jet0_corrmass" + label_ ] = df.loc[ :, "jet0_corrmass" ] * ( 1. - df.loc[ :, "jet0_unc" ] )
+            df.loc[ :, "jet0_px" + label_ ]       = ( df.loc[ :, "jet0_pt" + label_ ] * np.cos( df.loc[ :, "jet0_phi" ] ) )
+            df.loc[ :, "jet0_py" + label_ ]       = ( df.loc[ :, "jet0_pt" + label_ ] * np.sin( df.loc[ :, "jet0_phi" ] ) )
+            df.loc[ :, "jet0_pz" + label_ ]       = ( df.loc[ :, "jet0_pt" + label_ ] * np.sinh( df.loc[ :, "jet0_eta" ] ) )
 
     def calculateMuons( self, df ):
         label_ = "_nom"
@@ -66,6 +71,7 @@ class ProcessData:
         df.loc[ :, "muon0_px" + label_ ]     = ( df.loc[ :, "muon0_pt" + label_ ] * np.cos( df.loc[ :, "muon0_phi" ] ) )
         df.loc[ :, "muon0_py" + label_ ]     = ( df.loc[ :, "muon0_pt" + label_ ] * np.sin( df.loc[ :, "muon0_phi" ] ) )
         df.loc[ :, "muon0_pz" + label_ ]     = ( df.loc[ :, "muon0_pt" + label_ ] * np.sinh( df.loc[ :, "muon0_eta" ] ) )
+        if self.runOnMC_: pass
 
     def calculateWLep( self, df ):
         label_ = "_nom"
@@ -78,6 +84,7 @@ class ProcessData:
                                                  df.loc[ :, "WLeptonicPx" + label_ ]**2 -
                                                  df.loc[ :, "WLeptonicPy" + label_ ]**2 -
                                                  df.loc[ :, "WLeptonicPz" + label_ ]**2 ) ) 
+        if self.runOnMC_: pass
 
     def calculateWW( self, df ):
         label_ = "_nom"
@@ -88,24 +95,25 @@ class ProcessData:
                                                    ( df.loc[ :, "WLeptonicPy" + label_ ] + df.loc[ :, "jet0_py" + label_ ] )**2 -
                                                    ( df.loc[ :, "WW_pz" + label_ ] )**2 ) )
         df.loc[ :, "YWW" + label_ ]   = ( 0.5 * np.log( ( df.loc[ :, "WW_energy" + label_ ] + df.loc[ :, "WW_pz" + label_ ] ) / ( df.loc[ :, "WW_energy" + label_ ] - df.loc[ :, "WW_pz" + label_ ] ) ) )
-        label_ = "_jes_up"
-        df.loc[ :, "WW_energy" + label_ ]  = ( df.loc[ :, "WLeptonicE" + "_nom" ] + df.loc[ :, "jet0_energy" + label_ ] )
-        df.loc[ :, "WW_pz" + label_ ]      = ( df.loc[ :, "WLeptonicPz" + "_nom" ] + df.loc[ :, "jet0_pz" + label_ ] )
-        df.loc[ :, "MWW" + label_ ]   = ( np.sqrt( ( df.loc[ :, "WW_energy" + "_nom" ] )**2 -
-                                                   ( df.loc[ :, "WLeptonicPx" + "_nom" ] + df.loc[ :, "jet0_px" + label_ ] )**2 -
-                                                   ( df.loc[ :, "WLeptonicPy" + "_nom" ] + df.loc[ :, "jet0_py" + label_ ] )**2 -
-                                                   ( df.loc[ :, "WW_pz" + "_nom" ] )**2 ) )
-        df.loc[ :, "YWW" + label_ ]   = ( 0.5 * np.log( ( df.loc[ :, "WW_energy" + label_ ] + df.loc[ :, "WW_pz" + label_ ] ) / ( df.loc[ :, "WW_energy" + label_ ] - df.loc[ :, "WW_pz" + label_ ] ) ) )
-        label_ = "_jes_dw"
-        df.loc[ :, "WW_energy" + label_ ]  = ( df.loc[ :, "WLeptonicE" + "_nom" ] + df.loc[ :, "jet0_energy" + label_ ] )
-        df.loc[ :, "WW_pz" + label_ ]      = ( df.loc[ :, "WLeptonicPz" + "_nom" ] + df.loc[ :, "jet0_pz" + label_ ] )
-        df.loc[ :, "MWW" + label_ ]   = ( np.sqrt( ( df.loc[ :, "WW_energy" + "_nom" ] )**2 -
-                                                   ( df.loc[ :, "WLeptonicPx" + "_nom" ] + df.loc[ :, "jet0_px" + label_ ] )**2 -
-                                                   ( df.loc[ :, "WLeptonicPy" + "_nom" ] + df.loc[ :, "jet0_py" + label_ ] )**2 -
-                                                   ( df.loc[ :, "WW_pz" + "_nom" ] )**2 ) )
-        df.loc[ :, "YWW" + label_ ]   = ( 0.5 * np.log( ( df.loc[ :, "WW_energy" + label_ ] + df.loc[ :, "WW_pz" + label_ ] ) / ( df.loc[ :, "WW_energy" + label_ ] - df.loc[ :, "WW_pz" + label_ ] ) ) )
+        if self.runOnMC_:
+            label_ = "_jes_up"
+            df.loc[ :, "WW_energy" + label_ ]  = ( df.loc[ :, "WLeptonicE" + "_nom" ] + df.loc[ :, "jet0_energy" + label_ ] )
+            df.loc[ :, "WW_pz" + label_ ]      = ( df.loc[ :, "WLeptonicPz" + "_nom" ] + df.loc[ :, "jet0_pz" + label_ ] )
+            df.loc[ :, "MWW" + label_ ]   = ( np.sqrt( ( df.loc[ :, "WW_energy" + "_nom" ] )**2 -
+                                                       ( df.loc[ :, "WLeptonicPx" + "_nom" ] + df.loc[ :, "jet0_px" + label_ ] )**2 -
+                                                       ( df.loc[ :, "WLeptonicPy" + "_nom" ] + df.loc[ :, "jet0_py" + label_ ] )**2 -
+                                                       ( df.loc[ :, "WW_pz" + "_nom" ] )**2 ) )
+            df.loc[ :, "YWW" + label_ ]   = ( 0.5 * np.log( ( df.loc[ :, "WW_energy" + label_ ] + df.loc[ :, "WW_pz" + label_ ] ) / ( df.loc[ :, "WW_energy" + label_ ] - df.loc[ :, "WW_pz" + label_ ] ) ) )
+            label_ = "_jes_dw"
+            df.loc[ :, "WW_energy" + label_ ]  = ( df.loc[ :, "WLeptonicE" + "_nom" ] + df.loc[ :, "jet0_energy" + label_ ] )
+            df.loc[ :, "WW_pz" + label_ ]      = ( df.loc[ :, "WLeptonicPz" + "_nom" ] + df.loc[ :, "jet0_pz" + label_ ] )
+            df.loc[ :, "MWW" + label_ ]   = ( np.sqrt( ( df.loc[ :, "WW_energy" + "_nom" ] )**2 -
+                                                       ( df.loc[ :, "WLeptonicPx" + "_nom" ] + df.loc[ :, "jet0_px" + label_ ] )**2 -
+                                                       ( df.loc[ :, "WLeptonicPy" + "_nom" ] + df.loc[ :, "jet0_py" + label_ ] )**2 -
+                                                       ( df.loc[ :, "WW_pz" + "_nom" ] )**2 ) )
+            df.loc[ :, "YWW" + label_ ]   = ( 0.5 * np.log( ( df.loc[ :, "WW_energy" + label_ ] + df.loc[ :, "WW_pz" + label_ ] ) / ( df.loc[ :, "WW_energy" + label_ ] - df.loc[ :, "WW_pz" + label_ ] ) ) )
         
-    def __call__( self, apply_fiducial=True, within_aperture=False, random_protons=False, mix_protons=False, select_2protons=True, runOnMC=False ):
+    def __call__( self, apply_fiducial=True, within_aperture=False, random_protons=False, mix_protons=False, select_2protons=True ):
 
         df_counts = {}
         df_protons_multiRP_index = {}
@@ -120,11 +128,12 @@ class ProcessData:
         
                 df_counts_, df_protons_multiRP_, df_protons_singleRP_, df_ppstracks_ = get_data( self.fileNames_[ label_ ] )
 
-                f_jecUncertainty_ = lambda row_: self.getJetUncertainty( row_[ "jet0_eta" ], row_[ "jet0_pt" ] )
-                df_protons_multiRP_.loc[ :, "jet0_unc" ] = df_protons_multiRP_[ [ "jet0_pt", "jet0_eta" ] ].apply( f_jecUncertainty_, axis=1 )
-                print ( df_protons_multiRP_.loc[ :, "jet0_unc" ] )
-                # df_protons_multiRP_.loc[ :, "jet0_pt_up" ] = df_protons_multiRP_.loc[ :, "jet0_pt" ] * ( 1. + df_protons_multiRP_.loc[ :, "jet0_unc" ] )
-                # df_protons_multiRP_.loc[ :, "jet0_pt_dw" ] = df_protons_multiRP_.loc[ :, "jet0_pt" ] * ( 1. - df_protons_multiRP_.loc[ :, "jet0_unc" ] )
+                if self.runOnMC_:
+                    f_jecUncertainty_ = lambda row_: self.getJetUncertainty( row_[ "jet0_eta" ], row_[ "jet0_pt" ] )
+                    df_protons_multiRP_.loc[ :, "jet0_unc" ] = df_protons_multiRP_[ [ "jet0_pt", "jet0_eta" ] ].apply( f_jecUncertainty_, axis=1 )
+                    print ( df_protons_multiRP_.loc[ :, "jet0_unc" ] )
+                    # df_protons_multiRP_.loc[ :, "jet0_pt_up" ] = df_protons_multiRP_.loc[ :, "jet0_pt" ] * ( 1. + df_protons_multiRP_.loc[ :, "jet0_unc" ] )
+                    # df_protons_multiRP_.loc[ :, "jet0_pt_dw" ] = df_protons_multiRP_.loc[ :, "jet0_pt" ] * ( 1. - df_protons_multiRP_.loc[ :, "jet0_unc" ] )
                 self.calculateJets( df_protons_multiRP_ )
                 self.calculateMuons( df_protons_multiRP_ )
                 self.calculateWLep( df_protons_multiRP_ )
@@ -137,7 +146,7 @@ class ProcessData:
                     random_protons=random_protons,
                     mix_protons=mix_protons,
                     select_2protons=select_2protons,
-                    runOnMC=runOnMC
+                    runOnMC=self.runOnMC_
                     )
 
                 store_[ "counts" ] = df_counts_
--- process_signal.py
+++ process_signal.py
@@ -8,7 +8,7 @@ fileNames_signals = {
     "GGToWW-AQGC-A0W5e-6": [ "output-GGToWW-AQGC-A0W5e-6.h5" ]
     }
 
-process_data_ = ProcessData( labels=labels_signals, fileNames=fileNames_signals )
+process_data_ = ProcessData( labels=labels_signals, fileNames=fileNames_signals, runOnMC=True )
 
 labels_signals_mix_protons = [ "GGToWW-AQGC-mix_protons-A0W1e-6", "GGToWW-AQGC-mix_protons-A0W2e-6", "GGToWW-AQGC-mix_protons-A0W5e-6" ]
 
@@ -18,8 +18,8 @@ fileNames_signals_mix_protons = {
     "GGToWW-AQGC-mix_protons-A0W5e-6": [ "output-GGToWW-AQGC-mix_protons-A0W5e-6.h5" ]
     }
 
-process_data_mix_protons_ = ProcessData( labels=labels_signals_mix_protons, fileNames=fileNames_signals_mix_protons )
+process_data_mix_protons_ = ProcessData( labels=labels_signals_mix_protons, fileNames=fileNames_signals_mix_protons, runOnMC=True )
 
-process_data_( apply_fiducial=True, within_aperture=True, select_2protons=False, runOnMC=True )
+process_data_( apply_fiducial=True, within_aperture=True, select_2protons=False )
 
-process_data_mix_protons_( apply_fiducial=True, within_aperture=True, mix_protons=True, select_2protons=False, runOnMC=True )
+process_data_mix_protons_( apply_fiducial=True, within_aperture=True, mix_protons=True, select_2protons=False )
